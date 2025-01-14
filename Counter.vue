d07670e0ff070cbbde28ac2538d660f053ee8e7b
ma-farhan <160205034+ma-farhan@users.noreply.github.com>
2025-01-14 12:58:07 +0530

    Add files via upload

---
+++ Counter.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <p>Counter: {{ count }}</p>
+        <button @click="Increment">Increment</button>&nbsp;
+        <button @click="Decrement">Decrement</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const count=ref(0)
+
+function Increment() {
+    count.value++
+}
+
+function Decrement(){
+    count.value--
+}
+</script>
+
+<style scoped>
+div{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+p{
+    text-align: center;
+}
+</style>
